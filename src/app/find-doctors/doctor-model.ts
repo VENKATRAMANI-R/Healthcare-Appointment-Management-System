e872694538bc6df36d50ea1045eeb7fbaa1ef4b2
aravindhUser <Aravindh.R2@cognizant.com>
2025-09-22 17:28:56 +0530

    Aravindh Changes 22/09/2025

---
+++ src/app/find-doctors/doctor-model.ts
@@ -0,0 +1,10 @@
+// src/app/doctor.model.ts
+export interface Doctor {
+  name: string;
+  position: string;
+  qualification: string;
+  image: string;
+  hours: string[];
+  specialties: string[];
+  availability: string[];
+}
