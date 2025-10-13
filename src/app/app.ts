72d952acb97fbc27c02abb2c0da472a6d14f048c
VENKATRAMANI-R <Venkatramani.R@cognizant.com>
2025-10-13 15:16:59 +0530

    Venkat Commits Landing Page 2.0 13/10/2025

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { CommonModule, NgOptimizedImage } from "@angular/common";
-import { BookAppointment } from './book-appointment/book-appointment';
-import { MatDialog } from '@angular/material/dialog';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
 
+import { Header } from "./header/header";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, NgOptimizedImage, CommonModule],
+  imports: [RouterOutlet, CommonModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('HealthcareAppointmentManagementSystem');
-   isDropdownOpen = false;
-   isDropdownOpenProfile = false;
-
-  toggleDropdown(state: boolean): void {
-    this.isDropdownOpen = state;
-  }
-  toggleDropdownProfile(state: boolean): void {
-    this.isDropdownOpenProfile = state;
-  }
-  constructor(private dialog: MatDialog) {}
-
-  openAppointmentModal(): void {
-    this.dialog.open(BookAppointment, {
-      width: '600px'
-    });
-  }
+  constructor(public router: Router) {}
+  
 }
