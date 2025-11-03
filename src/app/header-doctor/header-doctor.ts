7c302fbdca1a06081473a8d56b49fead8d23cae9
VENKATRAMANI-R <Venkatramani.R@cognizant.com>
2025-11-03 17:02:23 +0530

    Doc Land Venks

--- src/app/header-doctor/header-doctor.ts
+++ src/app/header-doctor/header-doctor.ts
@@ -1,35 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit, NgModule, HostListener } from '@angular/core';
+import { Component} from '@angular/core';
 import { Router } from '@angular/router';
-import { ScheduleService } from '../schedule.service';
-import { DoctorService } from '../doctor-service';
-import { Subscription } from 'rxjs';
-export interface Doctor { 
-  doctorId: number;
-  firstName: string; 
-  lastName: string; 
-  specialty: string; 
-  email: string; 
-} 
-
 @Component({
   selector: 'app-header-doctor',
   imports: [CommonModule],
+  standalone: true,
   templateUrl: './header-doctor.html',
   styleUrls: ['./header-doctor.css']
 })
-export class HeaderDoctor implements OnInit {
-  doctor: Doctor | null = null;
-  private subs: Subscription[] = [];
+export class HeaderDoctor {
   isScrolled=false;
-  doctorName = "";
   AccountDropdownState = false;
-  constructor(private doctorService: DoctorService, private scheduleService: ScheduleService, private router: Router) {}
+  constructor(private router: Router) {}
   
-  ngOnInit(): void {
-    this.doctorName=localStorage.getItem('doctorName')||'';
-    
-  } 
   currentDate = new Date(); 
   toasts: Array<{id: string, message: string, type: string}> = []; 
   navigateTo(path: string): void {
@@ -59,17 +42,7 @@ export class HeaderDoctor implements OnInit {
   listConsultation():void{
     this.router.navigate(['/doctor-consultations']);
   }
-  loadDoctor(): void {
-    const sub = this.scheduleService.getDoctorProfile().subscribe({
-      next: (doc: Doctor | null) => {
-        this.doctor = doc;
-      },
-      error: (err: any) => {
-        console.error('Failed to load doctor profile', err);
-      }
-    });
-    this.subs.push(sub);
-  }
+  
   logout()
   {
     this.router.navigate(['/login-doctor']);
@@ -77,9 +50,5 @@ export class HeaderDoctor implements OnInit {
   goToUserProfile()
   {
     this.router.navigate(['/doctorProfiles']);
-  }
-  @HostListener('window:scroll')
-  onWindowScroll(): void {
-    this.isScrolled = window.pageYOffset > 100;
   }
 }
